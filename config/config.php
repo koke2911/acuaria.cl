728c070cf1c17b82b2add526d31c044ba9fcfd50
victor <victor.martinez@saludohiggins.cl>
2025-10-21 09:00:31 -0300

    conexiones

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 
-// $AUTH_URL="https://gestionapr.cl/public/Auth/create";
-// $LOGIN_URL= "https://gestionapr.cl/public/AquiaIA/consulta_login";
+$AUTH_URL="https://gestionapr.cl/public/Auth/create";
+$LOGIN_URL= "https://gestionapr.cl/public/AquiaIA/consulta_login";
 
 
-$AUTH_URL = "http://10.8.91.247:8080/public_html/public/Auth/create";
-$LOGIN_URL = "http://10.8.91.247:8080/public_html/public/AquiaIA/consulta_login";
+// $AUTH_URL = "http://10.8.91.247:8080/public_html/public/Auth/create";
+// $LOGIN_URL = "http://10.8.91.247:8080/public_html/public/AquiaIA/consulta_login";
 
 
 $API_ESTADISTICA= "https://api.acuaria.gestionapr.cl/estadisticas";
